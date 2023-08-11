23868db413b236080e12954d1a83d681b87434a3
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-08-11 17:25:25 +0200

    added new components to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Softeam Services!</h1>
+    return (
+        <div>
+            <Header />
+            <Courses />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    return <h1>Softeam Self Service Learning</h1>
+}
+
+function Courses() {
+    return (
+        <div>
+            <h2>Our courses</h2>
+            <Tutorial />
+            <Tutorial />
+            <Tutorial />
+            <Tutorial />
+        </div>
+    );
+}
+
+function Footer() {
+    return React.createElement('footer', null, "Happy Learning!!")
+}
+
+function Tutorial() {
+    return (
+        <div>
+            <img src='tutorials/aws.jpg' alt="Amazon Web Services" />
+            <h2>Amazon Web Services</h2>
+            <p>Amazon Web Services offers reliable, scalable, and inexpensive cloud computing services. Free to join, pay only for what you use.</p>
+        </div>
+    );
 }
 
 // React v18
