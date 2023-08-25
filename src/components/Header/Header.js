bb8b2cc9b7f81d3688b80f74b30c3901ad0327b1
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-08-25 16:20:33 +0200

    added Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Softeam Self Service Learning</h1>
+        </header>
+    );
+}
+
+export default Header;
