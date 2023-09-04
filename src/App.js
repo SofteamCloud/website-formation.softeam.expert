85e2ea0b157fe441ba710d647fffc2c50a893f5d
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-09-04 16:21:52 +0200

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Courses from './components/Courses/Courses';
+import Category from './components/Categories/Category';
+import Topic from './components/Categories/Topic';
+import Level from './components/Categories/Level';
+import MarkdownDisplay from './components/Categories/MarkdownDisplay';
 
 function App() {
     return (
-        <div className="container">
-            <Header />
-            <Courses />
-            <Footer />
-        </div>
+        <Router>
+            <div className="container">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Courses />} exact />
+                    <Route path="/category" element={<Category />} />
+                    <Route path="/:courseName" element={<Level />} />
+                    <Route path="/:courseName/:level" element={<Topic />} />
+                    <Route path="/:courseName/:level/:mdFileName" element={<MarkdownDisplay />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
