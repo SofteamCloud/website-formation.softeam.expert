0bde480ac357f9dd9e678c96f0d296d824fe1816
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-09-07 17:32:47 +0200

    added Level.css

--- src/components/Categories/Level.js
+++ src/components/Categories/Level.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import './Level.css';
 
 function Level() {
     const { courseName } = useParams();
     const levels = ['fundamental', 'essential', 'professional'];
 
     return (
-        <div>
+        <div className='level'>
             <h1>{courseName.toUpperCase()}</h1>
             <ul>
                 {levels.map(level => (
