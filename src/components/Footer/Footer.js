b6eb8b78a27b23239dcba43b115f142ca7d20140
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-09-07 17:35:26 +0200

    updated Footer.css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,9 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <footer className="footer">
-            <div className="container">
-                <img className="logo-softeam" src="./logos/softeam_logo.png" alt="logo" />
-            </div>
-        </footer>
+        <div className="footer">
+            <img className="logo-softeam" src="./logos/softeam_logo.png" alt="logo" />
+        </div>
     );
 }
 
