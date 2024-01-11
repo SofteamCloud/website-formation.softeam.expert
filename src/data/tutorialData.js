e7d6c0fb81deb6d2f8558eaf51ec7bbbc3e568ed
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-01-11 15:06:32 +0100

    updated tutorialData.js

--- src/data/tutorialData.js
+++ src/data/tutorialData.js
@@ -2,22 +2,22 @@ const tutorialData = [
     {
         name: "Amazon Web Services",
         overview: "Amazon Web Services offers reliable, scalable, and inexpensive cloud computing services. Free to join, pay only for what you use.",
-        photoName: "images/aws.jpg",
+        photoName: "images/aws.png",
     },
     {
         name: "Terraform Hashicorp",
         overview: "Terraform is an infrastructure as code tool that enables you to safely and predictably provision and manage infrastructure in any cloud.",
-        photoName: "images/terraform.jpg",
+        photoName: "images/terraform.png",
     },
     {
         name: "Kubernetes",
         overview: "Kubernetes is a portable, extensible, open source platform for managing containerized workloads and services, that facilitates both declarative configuration and automation.",
-        photoName: "images/kubernetes.jpg",
+        photoName: "images/kubernetes.png",
     },
     {
         name: "GitLab CI",
         overview: "GitLab is a widely used platform that enables teams to collaborate on software development projects. It provides a seamless interface for managing repositories, pipelines, and everything related to your project. ",
-        photoName: "images/gitlab.jpg",
+        photoName: "images/gitlab.png",
     },
 ];
 
