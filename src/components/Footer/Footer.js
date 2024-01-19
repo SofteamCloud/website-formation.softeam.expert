fafa8136b82c6317bbef27abf93366c1b33ceec6
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-01-19 16:47:28 +0100

    fixed logo display issue by updating image path to absolute

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer">
-            <img className="logo-softeam" src="./logos/softeam_white_transparent_logo.png" alt="logo" />
+            <img className="logo-softeam" src="/logos/softeam_white_transparent_logo.png" alt="logo" />
         </div>
     );
 }
