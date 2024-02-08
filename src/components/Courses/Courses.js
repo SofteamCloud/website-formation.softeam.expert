5900c257468e7360020df70e3c2e3f34f4a3c7df
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-02-08 16:51:12 +0100

    updated Courses page

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 function Courses() {
     return (
         <main className="course">
-            <h2>Our courses</h2>
             <p>At Softeam, we believe in the relentless pursuit of knowledge. Recognizing the rapidly evolving tech landscape, we introduced the Softeam Self Learning Service - a dedicated platform for our team members. This service offers a comprehensive range of courses and tutorials tailored for those eager to stay at the forefront of technology.</p>
 
             <ul className="tutorials">
@@ -15,11 +14,6 @@ function Courses() {
                     <Tutorial tutorialObj={tutorial} key={tutorial.name} />
                 ))}
             </ul>
-            <div>
-                <Link to="/category">
-                    <button className="btn">Happy Learning!</button>
-                </Link>
-            </div>
         </main>
     );
 }
