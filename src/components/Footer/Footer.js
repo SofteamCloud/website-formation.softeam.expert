14c322043ca7e42a78c3cc1299ae732d49544596
Minh Hung Phan <minhhung.phan2701@gmail.com>
2024-02-15 17:40:48 +0100

    updated Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,7 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer">
-            <p>Copyright © {new Date().getFullYear()} Softeam. All rights reserved.</p>
-            <p>This project is licensed under the WTFPL License.</p>
+            <p>Copyright © {new Date().getFullYear()} Softeam.</p>
             <p>Website developed by Minh Hung Phan.</p>
             <p>Contact us: <a href="mailto:ld-cloud@softeam.fr">ld-cloud@softeam.fr</a></p>
         </div>
